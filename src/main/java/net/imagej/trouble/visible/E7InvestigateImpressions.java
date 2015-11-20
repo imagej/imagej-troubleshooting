30d556d449b78592ddeed3c7a5ff6e0f73144268
Mark Hiner <hinerm@gmail.com>
2015-11-20 13:51:31 -0600

    Create Exercise 7

---
+++ src/main/java/net/imagej/trouble/visible/E7InvestigateImpressions.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * ImageJ interactive debugging tutorials.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of Wisconsin-Madison
+ * %%
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ * 
+ * See the CC0 1.0 Universal license for details:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * #L%
+ */
+
+package net.imagej.trouble.visible;
+
+import net.imagej.trouble.hidden.ObjectMaker;
+
+/**
+ * Exercise 7: Looking for memory leaks to identify the cause of
+ * {@link OutOfMemoryError} exceptions.
+ *
+ * @author Mark Hiner <hinerm@gmail.com>
+ */
+public class E7InvestigateImpressions {
+
+	public static void main(final String... args) {
+
+		final int size = Math.abs((int) Runtime.getRuntime().totalMemory() / 10);
+
+		// create 100 arrays of the determined size
+		// One of these may be a problem... but which?
+		for (int i = 0; i < 100; i++) {
+			System.out.println(ObjectMaker.getFloatArray(size));
+			System.out.println(ObjectMaker.getLongArray(size));
+			System.out.println(ObjectMaker.getDoubleArray(size));
+		}
+	}
+}
