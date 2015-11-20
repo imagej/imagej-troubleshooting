946592cf0216cf39ad78b0677dc3864ff2550c87
Mark Hiner <hinerm@gmail.com>
2015-11-20 13:51:18 -0600

    Create Exercise 6

---
+++ src/main/java/net/imagej/trouble/visible/E6SleuthingSilence.java
@@ -0,0 +1,45 @@
+/*
+ * #%L
+ * ImageJ interactive debugging tutorials.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of Wisconsin-Madison
+ * %%
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ * 
+ * See the CC0 1.0 Universal license for details:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * #L%
+ */
+
+package net.imagej.trouble.visible;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.trouble.hidden.NotALoop;
+
+/**
+ * Exercise 6: Identifying the location of problematic code when applications
+ * quit unexpectedly and/or silently.
+ *
+ * @author Mark Hiner <hinerm@gmail.com>
+ */
+@Plugin(type = Command.class,
+	menuPath = "Plugins>Troubleshooting>E6 - Start Looping")
+public class E6SleuthingSilence implements Command {
+
+	@Override
+	public void run() {
+		// Lets do some looping.. or not?
+
+		NotALoop.dontLoopTwice();
+
+		NotALoop.dontLoopThrice();
+
+		NotALoop.dontLoopForever();
+
+		NotALoop.loopForever();
+	}
+
+}
