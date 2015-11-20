19003a2e99b153952a84bf0a43fa5d6baf170ee4
Mark Hiner <hinerm@gmail.com>
2015-11-20 14:09:07 -0600

    Create MathDoer black box

---
+++ src/main/java/net/imagej/trouble/hidden/MathDoer.java
@@ -0,0 +1,27 @@
+/*
+ * #%L
+ * ImageJ interactive debugging tutorials.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of Wisconsin-Madison
+ * %%
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ * 
+ * See the CC0 1.0 Universal license for details:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * #L%
+ */
+
+package net.imagej.trouble.hidden;
+
+/**
+ * STOP LOOKING AT THIS CLASS!! IT'S OFF LIMITS!
+ *
+ * @author Mark Hiner <hinerm@gmail.com>
+ */
+public class MathDoer {
+
+	public static int doMaths() {
+		return 1;
+	}
+}
