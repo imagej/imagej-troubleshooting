d8ae242ecc9b144df1b3d483a4572eba14608b26
Mark Hiner <hinerm@gmail.com>
2015-11-20 13:35:06 -0600

    Create Exercise 5

---
+++ src/main/java/net/imagej/trouble/visible/E5HistoricalHysteria.java
@@ -0,0 +1,35 @@
+/*
+ * #%L
+ * ImageJ interactive debugging tutorials.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of Wisconsin-Madison
+ * %%
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ * 
+ * See the CC0 1.0 Universal license for details:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * #L%
+ */
+
+package net.imagej.trouble.visible;
+
+import net.imagej.trouble.hidden.MathDoer;
+
+/**
+ * Exercise 5: Identifying problematic changes in a code's history, when
+ * standard debugging techniques are insufficient.
+ *
+ * @author Mark Hiner <hinerm@gmail.com>
+ */
+public class E5HistoricalHysteria {
+
+	public static void main(final String... args) {
+		final int number = MathDoer.doMaths();
+
+		if (number < 0) throw new IllegalArgumentException(
+			"Something terrible has happened!");
+
+		System.out.println("You are awesome at mathings.");
+	}
+}
