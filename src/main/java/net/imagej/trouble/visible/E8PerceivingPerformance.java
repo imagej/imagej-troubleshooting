8a7ded98663904391c1d0e95c06c8ba23287d1b8
Mark Hiner <hinerm@gmail.com>
2015-11-20 13:51:39 -0600

    Create Exercise 8

---
+++ src/main/java/net/imagej/trouble/visible/E8PerceivingPerformance.java
@@ -0,0 +1,38 @@
+/*
+ * #%L
+ * ImageJ interactive debugging tutorials.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of Wisconsin-Madison
+ * %%
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ * 
+ * See the CC0 1.0 Universal license for details:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * #L%
+ */
+
+package net.imagej.trouble.visible;
+
+import net.imagej.trouble.hidden.StuffDoer;
+
+/**
+ * Exercise 8: Monitoring performance of methods in real time.
+ *
+ * @author Mark Hiner <hinerm@gmail.com>
+ */
+public class E8PerceivingPerformance {
+
+	public static void main(final String... args) {
+		final long time = System.currentTimeMillis();
+
+		// Do stuff for a minute
+		while (System.currentTimeMillis() - time < 120000) {
+			// One of these methods takes longer than the other.
+			// Without changing or looking at source code, how can we determine
+			// which is slower?
+			StuffDoer.doStuff();
+			StuffDoer.doMoreStuff();
+		}
+	}
+}
